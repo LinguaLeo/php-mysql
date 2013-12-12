2c1090462fe612616bd4fdb7f18fbf563df783ca
Anthony Regeda <regeda@inbox.ru>
2013-12-13 02:41:21 +0400

    removed scalar assertion in castArray method

--- src/LinguaLeo/DataQuery/Criteria.php
+++ src/LinguaLeo/DataQuery/Criteria.php
@@ -90,9 +90,6 @@ class Criteria
         if (is_array($value)) {
             return $value;
         }
-        if (is_scalar($value) || is_null($value)) {
-            return $value = [$value];
-        }
-        throw new CriteriaException(sprintf('Unknown data type %s for writable value', gettype($value)));
+        return $value = [$value];
     }
 }
