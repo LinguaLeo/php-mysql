94b0498acb5fdb0429a95a2c6f361be5371b8198
Anthony Regeda <regeda@inbox.ru>
2013-12-23 19:20:58 +0400

    added MyArray

---
+++ src/LinguaLeo/MyArray/Query.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace LinguaLeo\MyArray;
+
+use LinguaLeo\DataQuery\Criteria;
+use LinguaLeo\DataQuery\QueryInterface;
+use LinguaLeo\DataQuery\Exception\QueryException;
+
+class Query implements QueryInterface
+{
+    private $table;
+
+    public function __construct(array $table = [])
+    {
+        $this->table = $table;
+    }
+
+    private function getRowsCount($table)
+    {
+        if (empty($table)) {
+            return 0;
+        }
+        return count(reset($table));
+    }
+
+    private function getRowsIndeces($conditions)
+    {
+        if (empty($conditions)) {
+            return false;
+        }
+        $indeces = [];
+        foreach ($conditions as $condition) {
+            list($column, $value, $comparison) = $condition;
+            if (!isset($this->table[$column])) {
+                throw new QueryException(sprintf('The %s column not found', $column));
+            }
+            foreach ($this->table[$column] as $index => $rowValue) {
+                if ($this->isEqual($rowValue, $value, $comparison)) {
+                    $indeces[$column][] = $index;
+                }
+            }
+        }
+        if (!$indeces) {
+            return [];
+        }
+        if (count($indeces) === 1) {
+            return reset($indeces);
+        }
+        return call_user_func_array('array_intersect', $indeces);
+    }
+
+    private function isEqual($rowValue, $conditionValue, $comparison)
+    {
+        switch ($comparison) {
+            case Criteria::IS_NOT_NULL: return null !== $rowValue;
+            case Criteria::IS_NULL: return null === $rowValue;
+            case Criteria::IN: return in_array($rowValue, (array)$conditionValue);
+            case Criteria::NOT_IN: return !in_array($rowValue, (array)$conditionValue);
+            case Criteria::EQUAL: return $rowValue == $conditionValue;
+            case Criteria::EQUAL_GREATER: return $rowValue >= $conditionValue;
+            case Criteria::EQUAL_LESS: return $rowValue <= $conditionValue;
+            case Criteria::GREATER: return $rowValue > $conditionValue;
+            case Criteria::LESS: return $rowValue < $conditionValue;
+            case Criteria::NOT_EQUAL: return $rowValue != $conditionValue;
+            default:
+                throw new QueryException(sprintf('Unsupported %s comparison operator', $comparison));
+        }
+    }
+
+    private function executeUpdate(Criteria $criteria, callable $processor)
+    {
+        if (!$criteria->fields) {
+            throw new QueryException('No fields for update statement');
+        }
+
+        $indeces = $this->getRowsIndeces($criteria->conditions);
+
+        $affectedRows = [];
+
+        if (false == $indeces) {
+            $indeces = array_keys(reset($this->table));
+        }
+
+        foreach ($criteria->fields as $i => $column) {
+            if (!isset($this->table[$column])) {
+                throw new QueryException(sprintf('The %s column not found', $column));
+            }
+            foreach ($indeces as $index) {
+                if (call_user_func($processor, $column, $index, $criteria->values[$i])) {
+                    $affectedRows[$index] = true;
+                }
+            }
+        }
+
+        return new Result(null, count($affectedRows));
+    }
+
+    public function delete(Criteria $criteria)
+    {
+        $indeces = $this->getRowsIndeces($criteria->conditions);
+        if (false === $indeces) {
+            $count = $this->getRowsCount($this->table);
+            $this->table = [];
+            return new Result(null, $count);
+        }
+
+        foreach ($this->table as &$column) {
+            foreach ($indeces as $index) {
+                unset($column[$index]);
+            }
+        }
+
+        return new Result(null, count($indeces));
+    }
+
+    public function increment(Criteria $criteria)
+    {
+        return $this->executeUpdate($criteria, function($column, $index, $value) {
+            $this->table[$column][$index] += $value;
+            return true;
+        });
+    }
+
+    private function &castArray(&$value)
+    {
+        if (is_array($value)) {
+            return $value;
+        }
+        return $value = [$value];
+    }
+
+    public function insert(Criteria $criteria)
+    {
+        $count = null;
+        foreach ((array)$criteria->fields as $index => $field) {
+            $value = $this->castArray($criteria->values[$index]);
+            if (empty($this->table[$field])) {
+                $this->table[$field] = $value;
+            } else {
+                $this->table[$field] = array_merge($this->table[$field], $value);
+            }
+            if (null === $count) {
+                $count = count($value);
+            } elseif ($count !== count($value)) {
+                throw new QueryException(sprintf('Wrong rows count in %s column', $field));
+            }
+        }
+        return new Result(null, $count);
+    }
+
+    public function select(Criteria $criteria)
+    {
+        $indeces = $this->getRowsIndeces($criteria->conditions);
+        $table = $this->getMappedTable($this->table, $criteria->fields);
+        if (false !== $indeces) {
+            $flippedIndeces = array_flip($indeces);
+            foreach ($table as &$column) {
+                $column = array_intersect_key($column, $flippedIndeces);
+            }
+        }
+        return new Result($table, $this->getRowsCount($table));
+    }
+
+    protected function getMappedTable($table, $fields)
+    {
+        if (empty($fields)) {
+            return $table;
+        }
+        return array_intersect_key($table, array_flip($fields));
+    }
+
+    public function update(Criteria $criteria)
+    {
+        return $this->executeUpdate($criteria, function($column, $index, $value) {
+            if ($this->table[$column][$index] != $value) {
+                $this->table[$column][$index] = $value;
+                return true;
+            }
+            return false;
+        });
+    }
+}
---
+++ tests/LinguaLeo/MyArray/QueryTest.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace LinguaLeo\MyArray;
+
+use LinguaLeo\DataQuery\Criteria;
+
+class QueryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Query
+     */
+    protected $query;
+
+    /**
+     * @var Criteria
+     */
+    protected $criteria;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->query = new Query();
+
+        $this->criteria = new Criteria('test', 'trololo');
+    }
+
+    protected function getQueryMock()
+    {
+        return new Query(['foo' => [100, 300, 500], 'bar' => [200, 400, 600]]);
+    }
+
+    public function testOneInsert()
+    {
+        $this->criteria->write(['foo' => 1, 'bar' => 2]);
+
+        $this->assertCount(1, $this->query->insert($this->criteria));
+    }
+
+    public function testTwoInserts()
+    {
+        $this->criteria->writePipe(['foo' => 1, 'bar' => 2]);
+        $this->criteria->writePipe(['foo' => 3, 'bar' => 4]);
+
+        $this->assertCount(2, $this->query->insert($this->criteria));
+    }
+
+    public function testTwoInsertQueries()
+    {
+        $this->criteria->write(['foo' => 1, 'bar' => 2]);
+        $this->assertCount(1, $this->query->insert($this->criteria));
+        $this->criteria->write(['foo' => 1, 'bar' => 2]);
+        $this->assertCount(1, $this->query->insert($this->criteria));
+    }
+
+    /**
+     * @expectedException \LinguaLeo\DataQuery\Exception\QueryException
+     */
+    public function testFailedRowsCountInsert()
+    {
+        $this->criteria->write(['foo' => [1, 3], 'bar' => 2]);
+
+        $this->query->insert($this->criteria);
+    }
+
+    public function testDeleteAll()
+    {
+        $this->assertCount(3, $this->getQueryMock()->delete($this->criteria));
+    }
+
+    /**
+     * @expectedException \LinguaLeo\DataQuery\Exception\QueryException
+     */
+    public function testDeleteByUnknownColumn()
+    {
+        $this->criteria->where('ololo', 1);
+        $this->getQueryMock()->delete($this->criteria);
+    }
+
+    public function testDeleteByOneColumn()
+    {
+        $this->criteria->where('foo', 100);
+        $this->assertCount(1, $this->getQueryMock()->delete($this->criteria));
+    }
+
+    public function testDeleteByGreaterCondition()
+    {
+        $this->criteria->where('foo', 100, Criteria::GREATER);
+        $this->criteria->where('bar', 600, Criteria::LESS);
+        $this->assertCount(1, $this->getQueryMock()->delete($this->criteria));
+    }
+
+    public function testDeleteOnEmptyTable()
+    {
+        $query = new Query();
+        $this->assertCount(0, $query->delete($this->criteria));
+    }
+
+    public function testUpdate()
+    {
+        $this->criteria->write(['foo' => 1000, 'bar' => 2000]);
+        $this->criteria->where('foo', 100);
+        $this->assertCount(1, $this->getQueryMock()->update($this->criteria));
+    }
+
+    /**
+     * @expectedException \LinguaLeo\DataQuery\Exception\QueryException
+     */
+    public function testUpdateNoFields()
+    {
+        $this->getQueryMock()->update($this->criteria);
+    }
+
+    /**
+     * @expectedException \LinguaLeo\DataQuery\Exception\QueryException
+     */
+    public function testUpdateUnknownFields()
+    {
+        $this->criteria->write(['ololo' => 1]);
+        $this->getQueryMock()->update($this->criteria);
+    }
+
+    public function testUpdateNoAffectedRows()
+    {
+        $this->criteria->write(['bar' => 200]);
+        $this->criteria->where('foo', 100);
+        $this->assertCount(0, $this->getQueryMock()->update($this->criteria));
+    }
+
+    public function testIncrement()
+    {
+        $this->criteria->write(['foo' => 1, 'bar' => -1]);
+        $this->assertCount(3, $this->getQueryMock()->increment($this->criteria));
+    }
+
+    public function testSelectColumn()
+    {
+        $this->assertSame([100, 300, 500], $this->getQueryMock()->select($this->criteria)->column(0));
+    }
+
+    public function testSelectKeyValue()
+    {
+        $this->assertSame(
+            [100 => 200, 300 => 400, 500 => 600],
+            $this->getQueryMock()->select($this->criteria)->keyValue()
+        );
+    }
+
+    public function testSelectMany()
+    {
+        $this->assertSame(
+            [
+                ['foo' => 100, 'bar' => 200],
+                ['foo' => 300, 'bar' => 400],
+                ['foo' => 500, 'bar' => 600]
+            ],
+            $this->getQueryMock()->select($this->criteria)->many()
+        );
+    }
+
+    public function testSelectOne()
+    {
+        $this->assertSame(
+            ['foo' => 100, 'bar' => 200],
+            $this->getQueryMock()->select($this->criteria)->one()
+        );
+    }
+
+    public function testSelectValue()
+    {
+        $this->assertSame(100, $this->getQueryMock()->select($this->criteria)->value('foo'));
+    }
+
+    public function testSelectTable()
+    {
+        $this->assertSame(
+            ['foo' => [100, 300, 500], 'bar' => [200, 400, 600]],
+            $this->getQueryMock()->select($this->criteria)->table()
+        );
+    }
+
+    public function testSelectMapping()
+    {
+        $this->criteria->read(['foo']);
+        $this->assertSame(
+            [
+                ['foo' => 100],
+                ['foo' => 300],
+                ['foo' => 500]
+            ],
+            $this->getQueryMock()->select($this->criteria)->many()
+        );
+    }
+
+    public function testSelectWithCondition()
+    {
+        $this->criteria->read(['foo']);
+        $this->criteria->where('foo', 500, Criteria::NOT_EQUAL);
+        $this->assertSame(
+            [
+                ['foo' => 100],
+                ['foo' => 300]
+            ],
+            $this->getQueryMock()->select($this->criteria)->many()
+        );
+    }
+
+    public function testSelectEmptyValue()
+    {
+        $this->criteria->where('foo', 1);
+        $this->assertFalse($this->getQueryMock()->select($this->criteria)->value('foo'));
+    }
+
+    public function testSelectUndefinedColumn()
+    {
+        $this->assertNull($this->getQueryMock()->select($this->criteria)->value('ololo'));
+    }
+
+    public function provideConditions()
+    {
+        return [
+            [1, Criteria::EQUAL, [1 => ['foo' => 1]]],
+            [1, Criteria::NOT_EQUAL, [0 => ['foo' => null], 2 => ['foo' => 2]]],
+            [1, Criteria::EQUAL_GREATER, [1 => ['foo' => 1], 2 => ['foo' => 2]]],
+            [1, Criteria::EQUAL_LESS, [0 => ['foo' => null], 1 => ['foo' => 1]]],
+            [1, Criteria::GREATER, [2 => ['foo' => 2]]],
+            [1, Criteria::LESS, [0 => ['foo' => null]]],
+            [null, Criteria::IS_NOT_NULL, [1 => ['foo' => 1], 2 => ['foo' => 2]]],
+            [null, Criteria::IS_NULL, [0 => ['foo' => null]]],
+            [[1,3], Criteria::IN, [1 => ['foo' => 1]]],
+            [[1,3], Criteria::NOT_IN, [0 => ['foo' => null], 2 => ['foo' => 2]]]
+        ];
+    }
+
+    /**
+     * @dataProvider provideConditions
+     */
+    public function testSelectWithConditions($value, $comparison, $expected)
+    {
+        $query = new Query(['foo' => [null, 1, 2]]);
+        $this->criteria->where('foo', $value, $comparison);
+        $this->assertSame($expected, $query->select($this->criteria)->many());
+    }
+
+    /**
+     * @expectedException \LinguaLeo\DataQuery\Exception\QueryException
+     */
+    public function testSelectWithUnknownCondition()
+    {
+        $this->criteria->where('foo', null, '#CUSTOM');
+        $this->getQueryMock()->select($this->criteria);
+    }
+}
